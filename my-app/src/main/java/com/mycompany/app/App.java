40958d0605e7ae246ebf6686776660e6cd5589a8
mohithsai18 <saikmohith.com>
2025-08-30 18:35:23 +0530

    personal information

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -7,16 +7,25 @@ import java.util.Scanner;
  */
 public class App {
 
+    App(){
+        System.out.println("hello all devops engg..");
+    }
     public String name(String name)
     {
         return "name : "+name;
+    }
+    public int age(int age)
+    {
+        return age;
     }
     public static void main(String[] args) {
         System.out.println("HELLO enter your name : ");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
+        int age = sc.nextInt();
         App app = new App();
         System.out.println(app.name(name));
+        System.out.println("age :"+app.age(age));
     }
     
 
